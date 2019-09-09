ae74e92352ed7b56b12fca5f23ae999e09a10254
robvoiles <50212268+robvoiles@users.noreply.github.com>
2019-09-08 22:10:28 -0400

    Add files via upload
    colors extended

--- scripts.js
+++ scripts.js
@@ -12,11 +12,11 @@ function buttonAddAmount(amtAdd) {
   }
   document.getElementsByTagName("progress").item(0).value = currHealth;
   document.getElementById("currentHealth").innerText = (currHealth) + "  /  " + (maxHealth);
-  if (currHealth <= (maxHealth / 2) 
+  if (currHealth <= (maxHealth / 2))
  {
     document.getElementById("progress").color = "blue"
   }
-  if (currHealth > (maxHealth / 2) 
+  if (currHealth > (maxHealth / 2) )
  {
     document.getElementById("p2").style.color = B80F0A;
   }
